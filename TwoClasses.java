4320d583661d158297b2d28ba3ef6ded3f47a1bc
BipeenH <49411979+BipeenH@users.noreply.github.com>
2023-03-07 22:56:13 +0530

    Add files via upload

---
+++ TwoClasses.java
@@ -0,0 +1,11 @@
+class E{
+	public static void main(String[] args){
+		System.out.println("Blank name test");
+	}
+}
+
+class F{
+	public static void main(String[] args){
+		System.out.println("Blank name test second block");
+	}
+}
